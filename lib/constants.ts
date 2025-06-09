ee2321f1b99b18c36284d5d063750324fb407182
MuchaSsak <MuchaSsak.tech+github@gmail.com>
2025-06-09 14:50:38 +0200

    fix: set production url

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,8 +1,6 @@
 import "dotenv/config";
 
 export const DATABASE_URL = process.env.DATABASE_URL;
-// export const DATABASE_URL =
-//   "postgresql://neondb_owner:npg_oRXJAW15NIBD@ep-gentle-fog-a2le37bh-pooler.eu-central-1.aws.neon.tech/neondb?sslmode=require";
 
 // Links
 export const GITHUB_REPO_LINK = "https://github.com/MuchaSsak/LingoType";
@@ -10,5 +8,4 @@ export const GITHUB_MATEUSZ_PROFILE_LINK = "https://github.com/MuchaSsak";
 export const GITHUB_FABIAN_PROFILE_LINK = "https://github.com/RocketPLN";
 export const WEBSITE_MONKEYTYPE = "https://monkeytype.com";
 export const WEBSITE_QUIZLET = "https://quizlet.com";
-// export const LINGOTYPE_PRODUCTION_URL = "https://lingotype.vercel.app";
-export const LINGOTYPE_PRODUCTION_URL = "https://localhost:3000"; // For testing
+export const LINGOTYPE_PRODUCTION_URL = "https://lingotype.vercel.app";
